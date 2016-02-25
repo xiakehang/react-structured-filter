c3328e4ba1669b859da6d3770bf53b7066c589e8
Yang Bai <yang_bai@rapid7.com>
2016-02-25 14:30:45 -0800

    Clean up versions and webpack output logs

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,9 @@
 
 var gulp = require('gulp');
 var gutil = require("gulp-util");
-var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
-var browserify = require('browserify');
-var watchify = require('watchify');
-var reactify = require('reactify');
-var concat = require('gulp-concat');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var webpackConfig = require('./webpack.config.dev');
-
-// This code is largely from: http://christianalfoni.github.io/javascript/2014/08/15/react-js-workflow.html
-gulp.task('browserify', function() {
-  var bundler = browserify({
-    entries: ['./example/main.js'], // Only need initial file, browserify finds the deps
-    transform: [reactify], // We want to convert JSX to normal javascript
-    debug: true, // Gives us sourcemapping
-    cache: {}, packageCache: {},
-    fullPaths: true, // Requirement of watchify
-    paths: ['./node_modules','./example','./src']
-  });
-  var watcher  = watchify(bundler);
-
-  return watcher
-  .on('update', function () { // When any files update
-    var updateStart = Date.now();
-    console.log('Updating!');
-    watcher.bundle() // Create new bundle that uses the cache for high performance
-    .pipe(source('main.js'))
-    // This is where you add uglifying etc.
-    .pipe(gulp.dest('example/demo'));
-    console.log('Updated!', (Date.now() - updateStart) + 'ms');
-  })
-  .bundle() // Create the initial bundle when starting the task
-  .pipe(source('main.js'))
-  .pipe(gulp.dest('example/demo'));
-});
-
 
 gulp.task('dev', function(callback){
 
@@ -48,6 +15,16 @@ gulp.task('dev', function(callback){
         // server and middleware options
         contentBase: './public/demo',
         publicPath: '/js/',
+        stats: {
+          // Config for minimal console.log mess.
+          assets: true,
+          colors: true,
+          version: false,
+          hash: false,
+          timings: false,
+          chunks: false,
+          chunkModules: false
+        }
     }).listen(8080, "localhost", function(err) {
         if(err) throw new gutil.PluginError("webpack-dev-server", err);
         // Server listening
@@ -59,6 +36,4 @@ gulp.task('dev', function(callback){
 
 });
 
-
-// Just running the two tasks
 gulp.task('default', ['dev']);
